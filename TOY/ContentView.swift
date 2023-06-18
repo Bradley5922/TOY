d40ab34907cf7b03e558b9fff1e0e272f02d0d52
Bradley Cable <bradley5922@icloud.com>
2023-06-18 15:33:13 +0100

    Initial Commit

---
+++ TOY/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TOY
+//
+//  Created by Bradley Cable on 18/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TOY/TOYApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOYApp.swift
+//  TOY
+//
+//  Created by Bradley Cable on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TOYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
